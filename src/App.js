3c8555d3d46529f5c5899abfbaf91aaf8cf140ee
Reimon Odakura <pwd2mmi2ig@yahoo.co.jp>
2021-09-03 08:44:43 +0900

    コンポーネント化

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1> こんにちは</h1>
+      <p> こんにちは</p>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const App = () => {
-  return (
-    <>
-      <h1> こんにちは</h1>
-      <p> こんにちは</p>
-    </>
-  );
-};
+import App from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
