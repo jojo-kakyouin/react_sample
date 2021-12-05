a234d98dde5b002ec2999cfa3b3775bef1d4e1ca
Reimon Odakura <pwd2mmi2ig@yahoo.co.jp>
2021-12-05 14:38:22 +0900

    prpps化

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
+import ColorfulMessage from "./compornents/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
-
-  const contentLedyStyle = {
-    color: "pink",
-    fontSize: "18px"
-  };
 
   return (
     <>
       <h1 style={{ color: "red" }}> こんにちは</h1>
-      <p style={contentStyle}> おげんきですか？</p>
-      <p style={contentLedyStyle}> 元気です</p>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
--- src/compornents/ColorfulMessage.jsx
+++ src/compornents/ColorfulMessage.jsx
@@ -1,4 +1,12 @@
 import React from "react";
 
-const ColoerfulMessage = () => {};
+const ColoerfulMessage = (props) => {
+  //console.log(props);
+  const { color, children } = props;
+  const contentStyle = {
+    color,
+    fontSize: "18px"
+  };
+  return <p style={contentStyle}>{children}</p>;
+};
 export default ColoerfulMessage;
