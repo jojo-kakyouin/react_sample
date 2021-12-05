3581b303d4b371ba97b64ae83089d3bd58cbdc6b
Reimon Odakura <pwd2mmi2ig@yahoo.co.jp>
2021-12-05 22:40:20 +0900

    顔文字オンオフ

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ const App = () => {
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+  const onClickSwitchFaceShowFlg = () => {
+    setFaceShowFlg(!faceShowFlg);
+  };
   const [num, setNum] = useState(0);
+  const [faceShowFlg, setFaceShowFlg] = useState(true);
+
   return (
     <>
       <h1 style={{ color: "red" }}> こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickSwitchFaceShowFlg}>on/off</button>
+
       <p>{num}</p>
+      {faceShowFlg && <p>＼(^o^)／</p>}
     </>
   );
 };
